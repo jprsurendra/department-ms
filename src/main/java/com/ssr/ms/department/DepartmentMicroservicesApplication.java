87978bd155ee7128072d90d9908e1fae20c0b25c
Surendra <jpr_surendra@yahoo.co.in>
2022-12-25 14:47:41 +0530

    Project configuration ...

---
+++ src/main/java/com/ssr/ms/department/DepartmentMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.ssr.ms.department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssr/ms/department/DepartmentMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssr.ms.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
