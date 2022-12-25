c55815404229d8e1c086d3bf8222c100d17278d9
Surendra <jpr_surendra@yahoo.co.in>
2022-12-25 18:31:45 +0530

    Project configuration done

---
+++ src/main/java/com/ssr/ms/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.ssr.ms.department.repository;
+
+import com.ssr.ms.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
